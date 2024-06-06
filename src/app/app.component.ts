84cff0caad0fc56fea45cefb493e3fb1bd94ce62
Rohit Hulle <rohithulle@hotmail.com>
2024-06-06 10:41:54 +0530

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tripegaui';
+  northStates: string[] =[];
+  southStates: string[] =[];
+  eastStates: string[] =[];
+  westStates: string[] =[];
+  utStates: string[] =[];
+
+  ngOnInit(){
+    this.northStates = ['Jammu & Kashmir', 'Himachal Pradesh', 'Punjab', 'Uttarakhand', 'Haryana', 'Delhi', 'Rajasthan', 'Uttar Pradesh'];
+    this.southStates = ['Andhra Pradesh', 'Karnataka', 'Kerala', 'Tamil Nadu', 'Telangana'];
+    this.eastStates = ['Arunachal Pradesh', 'Assam', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Sikkim', 'Tripura','Bihar', 'Jharkhand', 'Odisha', 'West Bengal'];
+    this.westStates = ['Gujarat', 'Madhya Pradesh', 'Maharashtra', 'Goa'];
+    this.utStates = ['Andaman and Nicobar', 'Chandigarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Jammu and Kashmir', 'Ladakh', 'Lakshadweep', 'Puducherry'];
+  }
 }
